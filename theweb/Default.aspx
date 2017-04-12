e115359d627e1b2a898145379df5aaaec6d5de80
ponymark <101316230@gms.tcu.edu.tw>
2017-04-12 19:48:44 +0800

    以管理者為主的網站設計
    行事曆

---
+++ theweb/Default.aspx
@@ -0,0 +1,72 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+    <title></title>
+
+    <!-- Bootstrap -->
+    <link href="bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet" />
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+    <link href="StyleSheet.css" rel="stylesheet" />
+</head>
+<body style="height: 100%; width: 100%;">
+    <script src="JavaScript.js"></script>
+    <form id="form1" runat="server">
+        <div style="display: inline-block;height: 100%;
+    width:100%;">
+        <div class="mybackground">
+            
+            <div class="mycenter" >
+                <input id="Button1" type="button" value="上個月" class="btn btn-info btn-block" />
+            </div>
+            <div class="mycenter" >
+                <p id="Label1"></p>
+            </div>
+            <div class="mycenter"  >
+                <input id="Button2" type="button" value="下個月" class="btn btn-info btn-block" />
+            </div>
+
+            <div style="display:inline-block; width:100%">
+            <table id="mycalendar" class="table table-bordered">
+                <tr id="basic">
+                    <td>星期日</td>
+                    <td>星期一</td>
+                    <td>星期二</td>
+                    <td>星期三</td>
+                    <td>星期四</td>
+                    <td>星期五</td>
+                    <td>星期六</td>
+                </tr>
+            </table>
+                </div>
+        </div>
+            <div style="display: inline-block; margin-right: -4px; margin-left: auto; width: 1%; height: 100%; background-color: #478978; position: absolute; right: 19%;left:80%">
+            </div>
+            <div style="display: inline-block; margin-right: -4px; margin-left: auto; width: 19%; height: 100%; background-color: #84dcc6; position: absolute; right: 4px;">
+                
+            </div>
+        </div>
+    </form>
+    <script>
+
+    </script>
+</body>
+</html>
---
+++ theweb/JavaScript.js
@@ -0,0 +1,137 @@
+﻿$(document).ready(function () {
+
+
+
+
+    var d = new Date();
+    var y = d.getFullYear();
+    var m = d.getMonth();
+    var dd = d.getDate();
+    var n = d.getDay();
+
+    $("#Label1").text(y + '/' + (m + 1));
+
+    initialize(y, m);
+
+    $("#Button1").click(f1);
+    $("#Button2").click(f2);
+
+    $("#mycalendar tr[id!='basic'] td").mouseenter(function () {
+        $(this).addClass("myenter");
+    });
+
+    $("#mycalendar tr[id!='basic'] td").mouseleave(function () {
+        $(this).removeClass("myenter");
+    });
+
+    $('[data-toggle="popover"]').popover({ html: true, container: 'body', title: "Header", content: "<input type=\"text\" name=\"test\" value=\"test\" />", placement: "auto" });
+});
+
+function initialize(y, m) {
+
+    var td = new Date(y, m, 1);//2017-0-1=>2017-1-1
+    var tn = td.getDay();//0-日 1-一 ... 6- 六
+
+    var tj = 1;
+    var wk = 0;
+
+    var ff = new Date(y, m + 1, 0).getDate();//2017-1-0=>2017-1=>31
+
+    while (tj <= ff) {
+
+
+
+        var tt = "";
+
+        if (wk == 0) {//第一周
+            for (var g = 1; g < 8; g++) {
+
+                if (g <= tn) {
+                    tt += "<td class=\"myb1\">&nbsp";
+                }
+                else {
+                    tt += '<td class=\"myn1\" data-toggle=\"popover\" >';
+                    tt += tj;
+                    tj++;
+                }
+                tt += '</td>';
+            }
+
+            $('#mycalendar tr:last').after('<tr id=\"' + "wk" + wk + '\">' + tt + '</tr>');
+        }
+        else {
+            var rr = tj + 7;
+            for (var g = tj; g < rr; g++) {
+
+
+                if (tj > ff) {
+                    tt += "<td class=\"myb1\">&nbsp";
+                }
+                else {
+                    tt += '<td class=\"myn1\" data-toggle=\"popover\" >';
+                    tt += tj;
+                }
+                tj++;
+                tt += '</td>';
+            }
+
+            $('#mycalendar tr:last').after('<tr id=\"'+"wk"+wk+'\">' + tt + '</tr>');
+        }
+
+        wk++;
+    }
+
+}
+function f1() {
+    //回上個月
+    $("[data-toggle='popover']").popover('destroy');
+    var y = $("#Label1").text().split('/')[0];
+    var m = $("#Label1").text().split('/')[1];
+    if (m - 1 == 0) {
+        m = 12;
+        $("#Label1").text(--y + '/' + m);
+        $("#mycalendar tr[id!='basic']").closest("tr").remove();
+        initialize(y, m - 1);
+    }
+    else {
+        $("#Label1").text(y + '/' + --m);
+        $("#mycalendar tr[id!='basic']").closest("tr").remove();
+        initialize(y, m - 1);
+    }
+
+
+    $("#mycalendar tr[id!='basic'] td").mouseenter(function () {
+        $(this).addClass("myenter");
+    });
+
+    $("#mycalendar tr[id!='basic'] td").mouseleave(function () {
+        $(this).removeClass("myenter");
+    });
+    $('[data-toggle="popover"]').popover({ html: true, container: 'body', title: "Header", content: "<input type=\"text\" name=\"test\" value=\"test\" />", placement: "auto" });
+}
+function f2() {
+    //回下個月
+    $("[data-toggle='popover']").popover('destroy');
+    var y = $("#Label1").text().split('/')[0];
+    var m = $("#Label1").text().split('/')[1];
+    if (m == 12) {
+        m = 1;
+        $("#Label1").text(++y + '/' + m);
+        $("#mycalendar tr[id!='basic']").closest("tr").remove();
+        initialize(y, m - 1);
+    }
+    else {
+        $("#Label1").text(y + '/' + ++m);
+        $("#mycalendar tr[id!='basic']").closest("tr").remove();
+        initialize(y, m - 1);
+    }
+
+    $("#mycalendar tr[id!='basic'] td").mouseenter(function () {
+        $(this).addClass("myenter");
+    });
+
+    $("#mycalendar tr[id!='basic'] td").mouseleave(function () {
+        $(this).removeClass("myenter");
+    });
+    $('[data-toggle="popover"]').popover({ html: true, container: 'body', title: "Header", content: "<input type=\"text\" name=\"test\" value=\"test\" />", placement: "auto" });
+}
